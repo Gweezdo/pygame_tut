1d23e4faa3e54c7e510757efe57090279b886aea
Zjak Theron <z.trivium@gmail.com>
2023-08-11 21:38:34 +1000

    Working Commit

--- main.py
+++ main.py
@@ -11,9 +11,15 @@ test_font = pygame.font.Font('font\Pixeltype.ttf', 50)
 #test_surface = pygame.Surface((100,200))
 #test_surface.fill('Red')
 
-sky_surface = pygame.image.load('graphics\Sky.png')
-ground_surface = pygame.image.load('graphics\ground.png')
-text_surface = test_font.render("Gamey game time!",True,'Red')
+sky_surface = pygame.image.load('graphics\Sky.png').convert()
+ground_surface = pygame.image.load('graphics\ground.png').convert()
+text_surface = test_font.render("Gamey game time!",False,'Red')
+
+snail_surf = pygame.image.load('graphics\snail\snail1.png').convert_alpha()
+snail_rect = snail_surf.get_rect(midbottom = (600,300))
+
+player_surf = pygame.image.load('graphics\Player\player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80,300))
 
 while True:
     for event in pygame.event.get():
@@ -24,6 +30,23 @@ while True:
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface, (0,300))
     screen.blit(text_surface, (300,50))
+    screen.blit(snail_surf,snail_rect)
+    screen.blit(player_surf, player_rect)
 
+    snail_rect.x -= 4
+    if snail_rect.right <= 0:
+        snail_rect.left = 800   
+    
+
+    # if player_rect.colliderect(snail_rect):
+    #     player_rect.x -= 40
+
+    # if player_rect.right <= 0:
+    #     player_rect.x = 300
+    mouse_pos = pygame.mouse.get_pos()
+    if player_rect.collidepoint((mouse_pos)):
+        print(pygame.mouse.get_pressed())
+
+    #player_rect.x += 1
     pygame.display.update()
     clock.tick(60)
